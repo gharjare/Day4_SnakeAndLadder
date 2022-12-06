f24483b2cfc4f29ea6d01442d40800c7564e8d3f
gharjare <gharjarechetan@gmail.com>
2022-12-06 16:54:39 +0530

    use switch cases

---
+++ UsingCases.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class UsingCases
+    {
+        public static void SwitchCase()
+        {
+            int position = 0;
+            int player = 1;
+
+
+            Random random = new Random();
+            int dieroll = random.Next(0, 7);
+
+            switch(dieroll)
+            {
+                case 1:
+                    Console.WriteLine("Play Snake");
+                    position -= 1;
+                    break;
+                case 2:
+                    Console.WriteLine("Play Ladder");
+                    position++;
+                    break;
+                    default:
+                    Console.WriteLine("No play");
+                    break;
+            }
+        }
+    }
+}
