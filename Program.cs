dc7833576f34fcc2a4777880d8675edb64b8bc52
gharjare <gharjarechetan@gmail.com>
2022-12-06 16:49:42 +0530

    Add program file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //DieRoll.Roll();
+            //RepeatPosition.CheckPosition();
+        }
+    }
+}
