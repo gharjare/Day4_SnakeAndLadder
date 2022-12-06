0326056a1b0c8f3a45469dbe1111c9ad0a51d3b5
gharjare <gharjarechetan@gmail.com>
2022-12-06 16:53:16 +0530

    roll die

---
+++ DieRoll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class DieRoll
+    {
+        public static void Roll()
+        {
+            
+            int position = 0;
+            int player = 1;
+           
+           
+                Random random = new Random();
+                int dieroll = random.Next(0, 7);
+
+                
+           
+            
+
+        }
+    }
+}
